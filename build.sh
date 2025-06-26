7ce4ce8eb54c95d78b5425af5fd30cdd75f18f67
Arasavilli Devika <arasavillidevika@gmail.com>
2025-06-26 21:19:47 +0530

    Fix: Make build.sh executable

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# Install dependencies except TensorFlow
-pip install -r requirements.txt || exit 1
-
-# Manually install a version of TensorFlow compatible with Python 3.10
-pip install tensorflow==2.10.0 || exit 1
+pip install -r requirements.txt
