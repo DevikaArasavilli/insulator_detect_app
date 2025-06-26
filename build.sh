8f5b30b1dece8e0279cebebb21ce4f14acfa5047
Arasavilli Devika <arasavillidevika@gmail.com>
2025-06-26 21:04:57 +0530

    Fix: Ensure custom build with TensorFlow 2.10.0 and Python 3.10

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install base dependencies
-pip install -r requirements.txt
+# Install dependencies except TensorFlow
+pip install -r requirements.txt || exit 1
 
-# Manually install TensorFlow compatible with Python 3.10
-pip install tensorflow==2.10.0
+# Manually install a version of TensorFlow compatible with Python 3.10
+pip install tensorflow==2.10.0 || exit 1
