425b3825559efd22a5747d585317686eead5a7d4
Arasavilli Devika <arasavillidevika@gmail.com>
2025-06-26 20:56:45 +0530

    Fix: Use Python 3.10 and install TF manually

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install regular dependencies
+# Install base dependencies
 pip install -r requirements.txt
 
-# Manually install compatible TensorFlow
+# Manually install TensorFlow compatible with Python 3.10
 pip install tensorflow==2.10.0
